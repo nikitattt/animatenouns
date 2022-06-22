904f864d0b00bbee98bf5dd95eba73d2384ad034
ng <0xng@proton.me>
2022-06-22 22:58:11 +0300

    Add tailwind and prettier spec

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="font-sans text-black-text flex flex-col h-screen">
       <Head>
         <title>Animate Nouns</title>
         <meta name="description" content="Animate your noun here" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Animate Your Lil Noun. Coming Soon.</h1>
+      <main className="mt-20 px-8">
+        <h1 className="font-display font-bold text-5xl text-red text-center">
+          Animate Your Lil Noun. Coming Soon.
+        </h1>
       </main>
 
-      <footer className={styles.footer}></footer>
+      <footer className=""></footer>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
