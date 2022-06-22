de4aeb51ce6cd93c39dc90d49be9dc7f7ae309c8
ng <0xng@proton.me>
2022-06-22 23:30:12 +0300

    Change Image class to img in navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
 const NavBar = () => {
   return (
     <div className="flex flex-row justify-between items-center px-8 py-4">
       <div className="h-14 w-14 relative">
-        <Image src="/icon.gif" layout="fill" />
+        <img src="/icon.gif" />
       </div>
       <div className="flex flex-row items-center gap-4">
         <div></div>
