30c5948206b80cee0fb2c22ede0f1cad04514d0c
ng <0xng@proton.me>
2022-06-26 01:48:39 +0300

    Update artist form page with hint if form doesn't load

--- pages/artist-form.tsx
+++ pages/artist-form.tsx
@@ -11,8 +11,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <NavBar />
       <main>
+        <p className="mt-8 sm:mt-20 text-2xl text-grey text-center">
+          If you don't see the form, refresh the page
+        </p>
         <iframe
           className="absolute top-0 bottom-0 left-0 right-0"
           src={process.env.ARTIST_FORM_LINK}
