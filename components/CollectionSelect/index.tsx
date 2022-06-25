ddf20697947ceeb76493ab8394ce185c8503e4e4
ng <0xng@proton.me>
2022-06-25 20:31:25 +0300

    Add base empty component for collection select

---
+++ components/CollectionSelect/index.tsx
@@ -0,0 +1,5 @@
+const CollectionSelect = () => {
+  return <div className=""></div>
+}
+
+export default CollectionSelect
--- components/NounLoadOrCreate/index.tsx
+++ components/NounLoadOrCreate/index.tsx
@@ -1,5 +1,11 @@
+import CollectionSelect from '../CollectionSelect'
+
 const NounLoadOrCreate = () => {
-  return <div className="flex flex-column"></div>
+  return (
+    <div className="flex flex-column">
+      <CollectionSelect />
+    </div>
+  )
 }
 
 export default NounLoadOrCreate
