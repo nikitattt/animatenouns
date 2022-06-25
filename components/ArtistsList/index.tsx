d08882752a988023fa63b1ef4921b95d3a936360
ng <0xng@proton.me>
2022-06-26 00:57:53 +0300

    Move artist box to separate component

--- components/ArtistsList/index.tsx
+++ components/ArtistsList/index.tsx
@@ -1,14 +1,13 @@
 import { Artist } from '../../utils/types/artist'
+import ArtistBox from '../ArtistBox'
 
 const ArtistsList = ({ artists }: { artists: Artist[] }) => {
   return (
     <div className="mt-8 flex flex-col gap-4">
       {artists.map(function (artist, place) {
         return (
-          <div key={place} className="w-full rounded-2xl bg-teal h-60">
-            <div className="mt-4 ml-4">
-              <p className="text-2xl font-semibold">{artist.name}</p>
-            </div>
+          <div key={place}>
+            <ArtistBox artist={artist} />
           </div>
         )
       })}
