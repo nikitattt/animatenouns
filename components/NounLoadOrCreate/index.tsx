3141ae6b876e954d5f1221866f0f6fdbe24b3664
ng <0xng@proton.me>
2022-06-25 20:33:21 +0300

    Add base empty component for selecting traits

--- components/NounLoadOrCreate/index.tsx
+++ components/NounLoadOrCreate/index.tsx
@@ -1,9 +1,11 @@
 import CollectionSelect from '../CollectionSelect'
+import TraitsSelect from '../TraitsSelect'
 
 const NounLoadOrCreate = () => {
   return (
     <div className="flex flex-column">
       <CollectionSelect />
+      <TraitsSelect />
     </div>
   )
 }
---
+++ components/TraitsSelect/index.tsx
@@ -0,0 +1,5 @@
+const TraitsSelect = () => {
+  return <div className=""></div>
+}
+
+export default TraitsSelect
