0547f3f13b631f50cbe7276996193390b1c72373
ng <0xng@proton.me>
2022-08-31 00:11:39 +0300

    Show loaded lilnoun

--- components/AnimateNoun/index.tsx
+++ components/AnimateNoun/index.tsx
@@ -1,12 +1,10 @@
-import { useNounStore } from '../../state/noun'
 import AnimationSelect from '../AnimationSelect'
 import NounPreview from '../NounPreview'
 import NounSelectAndActions from '../NounSelectAndActions'
 
 const AnimateNoun = () => {
   const animations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  const activeNoun = useNounStore((state) => state.activeNoun)
-  console.log(activeNoun)
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-col lg:flex-row mx-auto gap-4 w-full justify-center">
@@ -14,7 +12,7 @@ const AnimateNoun = () => {
           <p className="font-display text-grey-light text-4xl">Your Noun</p>
         </div> */}
         <div className="w-full lg:w-96">
-          <NounPreview animation={{}} seed={{}} />
+          <NounPreview />
         </div>
         <div className="w-full lg:w-96">
           <NounSelectAndActions />
--- components/NounPreview/index.tsx
+++ components/NounPreview/index.tsx
@@ -1,16 +1,45 @@
+import {
+  ImageData as LilImageData,
+  getNounData as getLilNounData
+} from '@lilnouns/assets'
+import { buildSVG } from '@nouns/sdk/dist/image/svg-builder'
 import clsx from 'clsx'
+import { useNounStore } from '../../state/noun'
+
+const { palette } = LilImageData
 
 type NounPreviewProps = {
-  animation: any
   animationActive?: boolean
-  seed: any
 }
 
 const NounPreview = ({
-  animation,
-  animationActive = true,
-  seed
+  animationActive = true
 }: Readonly<NounPreviewProps>) => {
+  const activeNoun = useNounStore((state) => state.activeNoun)
+
+  let image = <></>
+  if (activeNoun !== undefined) {
+    const { parts, background } = getLilNounData(activeNoun)
+    const svgBinary = buildSVG(parts, palette, background)
+    const svgBase64 = Buffer.from(svgBinary).toString('base64')
+
+    image = (
+      <img
+        className="w-full h-full rounded-2xl"
+        src={`data:image/svg+xml;base64,${svgBase64}`}
+      />
+    )
+  } else {
+    image = (
+      <p className="text-grey font-display font-thin text-2xl tracking-wide text-center">
+        Get started
+        <br />
+        on the right
+      </p>
+    )
+  }
+  console.log(activeNoun)
+
   return (
     <div
       className={clsx(
@@ -18,13 +47,7 @@ const NounPreview = ({
         'rounded-2xl aspect-square w-full'
       )}
     >
-      {animationActive && (
-        <p className="text-grey font-display font-thin text-2xl tracking-wide text-center">
-          Coming
-          <br />
-          soon!
-        </p>
-      )}
+      {animationActive && image}
     </div>
   )
 }
