dac143d2f01f453ebf191bd6433c1af9c4c1b32f
ng <0xng@proton.me>
2022-09-27 23:23:09 +0300

    Add save info text

---
+++ components/SaveNoun/index.tsx
@@ -0,0 +1,9 @@
+const SaveNoun = () => {
+  return (
+    <div className="text-black font-display text-center">
+      {'Right Click (📱long press) -> Save'}
+    </div>
+  )
+}
+
+export default SaveNoun
