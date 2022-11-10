cfc663627439d432df8374f72b2e5055963fddb8
ng <0xng@proton.me>
2022-11-10 21:02:44 +0200

    Cleanup

--- utils/animations/nouns.ts
+++ utils/animations/nouns.ts
@@ -431,7 +431,6 @@ export class NounsAnimations implements ClassicNounAnimations {
       }
     ]
 
-    // TODO: need to add 8.8 art. Require npm package update
     this.animationsIdMaps = [
       {
         supportedId: '0,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22',
