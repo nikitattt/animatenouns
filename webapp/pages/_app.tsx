89db4275dd96c8d5d5d58c1beeb7871f9451ef4c
ng <0xng@proton.me>
2022-12-23 17:32:54 +0200

    Add simpleanalytics script

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Script src="https://adata.animatenouns.wtf/latest.js" />
+    <noscript>
+      {/* eslint-disable @next/next/no-img-element */}
+      <img
+        src="https://adata.animatenouns.wtf/noscript.gif"
+        alt=""
+        referrerPolicy="no-referrer-when-downgrade"
+      />
+    </noscript>
+  </>
 }
 
 export default MyApp
